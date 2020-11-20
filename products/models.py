8459253a854c42f54e350f1754825de9629fb13e
Andrea Maggino <andreamaggino99@gmail.com>
2020-11-20 11:25:34 +0000

    Fixing image connection error with dj

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.FloatField(blank=False)
     category = models.ForeignKey('Category', blank=True, null=True, on_delete=models.SET_NULL)
-    image = models.ImageField(upload_to='images')
+    #image = models.ImageField(upload_to='images')
 
     def __str__(self):
         return self.name
